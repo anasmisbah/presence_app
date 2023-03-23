5b190aca894b9d8b1d4258b51b3cc2fc1bfefc0e
Anas Misbah <anas.misbahuddin4869@gmail.com>
2023-03-23 14:51:07 +0800

    add pegawai

---
+++ lib/app/modules/add_pegawai/controllers/add_pegawai_controller.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AddPegawaiController extends GetxController {
+  TextEditingController nameC = TextEditingController();
+  TextEditingController nipC = TextEditingController();
+  TextEditingController emailC = TextEditingController();
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  void addPegawai() async {
+    print("add pegawai");
+    if (nameC.text.isNotEmpty &&
+        nipC.text.isNotEmpty &&
+        emailC.text.isNotEmpty) {
+      try {
+        final userCredential = await auth.createUserWithEmailAndPassword(
+          email: emailC.text,
+          password: "qweasdzxc",
+        );
+
+        if (userCredential.user != null) {
+          String uid = userCredential.user!.uid;
+
+          firestore.collection("pegawai").doc(uid).set({
+            "nip": nipC.text,
+            "name": nameC.text,
+            "email": emailC.text,
+            "uid": uid,
+            "created_at": DateTime.now().toIso8601String(),
+          });
+        }
+
+        print(userCredential);
+      } on FirebaseAuthException catch (e) {
+        print(e.code);
+        if (e.code == 'weak-password') {
+          Get.snackbar(
+              'Terjadi Kesalahan', 'password yang digunakan terlalu singkat');
+        } else if (e.code == 'email-already-in-use') {
+          Get.snackbar('Terjadi Kesalahan',
+              'Pegawai telah terdaftar, tidak dapat menambahkan dengan email ini');
+        }
+      } catch (e) {
+        Get.snackbar('Terjadi Kesalahan', 'Tidak dapat menambahkan pegawai');
+      }
+    } else {
+      Get.snackbar('Terjadi Kesalahan', 'NIP, nama, dan email harus diisi');
+    }
+  }
+}
