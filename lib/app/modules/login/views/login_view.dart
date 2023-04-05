c58209fe29add32d2c7b9df68de37f7cd4b6596d
Anas Misbah <anas.misbahuddin4869@gmail.com>
2023-04-05 16:30:28 +0800

    update login page

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -10,55 +10,113 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        centerTitle: true,
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(20),
-        children: [
-          TextField(
-            autocorrect: false,
-            controller: controller.emailC,
-            decoration: InputDecoration(
-              labelText: "Email",
-              border: OutlineInputBorder(),
-            ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 25,
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    const Text(
+                      "Selamat Datang",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "Masuk dengan email dan password",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      autocorrect: false,
+                      controller: controller.emailC,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      autocorrect: false,
+                      controller: controller.passC,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Get.toNamed(Routes.FORGOT_PASSWORD);
+                      },
+                      child: const Text(
+                        "Lupa Password ? Reset disini",
+                        style: TextStyle(
+                          color: Color(0xFFEE701D),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Obx(
+                () => ElevatedButton(
+                  onPressed: () async {
+                    if (controller.isLoading.isFalse) {
+                      await controller.login();
+                    }
+                  },
+                  child: controller.isLoading.isFalse
+                      ? const Text(
+                          "Masuk",
+                          style: TextStyle(fontSize: 20),
+                        )
+                      : const Text(
+                          "Loading..",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFEE701D),
+                    minimumSize: Size(Get.width, 0),
+                    padding: const EdgeInsets.symmetric(vertical: 18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            autocorrect: false,
-            controller: controller.passC,
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: "Password",
-              border: OutlineInputBorder(),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Obx(
-            () => ElevatedButton(
-              onPressed: () async {
-                if (controller.isLoading.isFalse) {
-                  await controller.login();
-                }
-              },
-              child: controller.isLoading.isFalse
-                  ? Text("Login")
-                  : Text("Loading.."),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              Get.toNamed(Routes.FORGOT_PASSWORD);
-            },
-            child: Text("Lupa Password ?"),
-          ),
-        ],
+        ),
       ),
     );
   }
